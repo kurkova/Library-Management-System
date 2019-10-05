558d8364facf2dc564de3737a9c86f2006b6c31f
kurkova <kurkiewiczpaula@gmail.com>
2019-10-05 17:39:42 +0100

    connected with mysql

--- src/main/java/com/project/library/controller/BookController.java
+++ src/main/java/com/project/library/controller/BookController.java
@@ -6,18 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1")
 public class BookController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/bookTile")
     public void addBookTitle (BookTitleDto bookTitleDto){
-
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/bookCopy")
     public void addBookCopy (BookCopyDto bookCopyDto){
+    }
+
+    public List<BookCopyDto> getAvailableBooks (String title){
+        return new ArrayList<>();
+    }
+
+    public void returnBook(Long id){
 
     }
 
+    public void updateStatusBook(Long id){
+
+    }
 }
