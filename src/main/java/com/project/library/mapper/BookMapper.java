c0e5aff5462779186bf9800446ced93868e4e8b7
kurkova <kurkiewiczpaula@gmail.com>
2019-10-12 18:01:24 +0100

    added BookCopy maper

--- src/main/java/com/project/library/mapper/BookMapper.java
+++ src/main/java/com/project/library/mapper/BookMapper.java
@@ -1,8 +1,12 @@
 package com.project.library.mapper;
 
+import com.project.library.domain.BookCopy;
 import com.project.library.domain.BookTitle;
+import com.project.library.dto.BookCopyDto;
 import com.project.library.dto.BookTitleDto;
 import com.project.library.repository.BookTitleRepository;
+import com.project.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,12 +15,16 @@ import java.util.stream.Collectors;
 @Component
 public class BookMapper {
 
+    @Autowired
+    BookService bookService;
+
 //    public BookTitle mapToBookTitle (final BookTitleDto bookTitleDto){
 //        return new BookTitle(
 //                bookTitleDto.getId(),
 //                bookTitleDto.getTitle(),
 //                bookTitleDto.getAuthor(),
-//                bookTitleDto.getYearOfPublication()
+//                bookTitleDto.getYearOfPublication(),
+//
 //        );
 //    }
 
@@ -32,6 +40,28 @@ public class BookMapper {
     public List<BookTitleDto> mapToBookTitleDtoList (final List<BookTitle> bookTitlesList){
         return bookTitlesList.stream()
                 .map(b-> new BookTitleDto(b.getId(), b.getTitle(), b.getAuthor(), b.getYearOfPublication()))
+                .collect(Collectors.toList());
+    }
+
+//    public BookCopy mapToBookCopy (final BookCopyDto bookCopyDto){
+//        return new BookCopy(
+//                bookCopyDto.getId(),
+//                bookCopyDto.getBookTitleId(),
+//                bookCopyDto.getBookStatusId()
+//        );
+//    }
+
+    public BookCopyDto mapToBookCopyDto (final BookCopy bookCopy){
+        return new BookCopyDto(
+                bookCopy.getId(),
+                bookCopy.getBookTitleId(),
+                bookCopy.getBookStatus()
+        );
+    }
+
+    public List<BookCopyDto> mapToBookCopyDtoList (final List<BookCopy> bookCopyList){
+        return bookCopyList.stream()
+                .map(b -> new BookCopyDto(b.getId(), b.getBookTitleId(), b.getBookStatus()))
                 .collect(Collectors.toList());
     }
 
