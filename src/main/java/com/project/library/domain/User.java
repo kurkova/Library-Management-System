57acca81404677e4c345fba102cf20bf759255f6
kurkova <kurkiewiczpaula@gmail.com>
2019-12-17 17:25:37 +0100

    Refactoring

--- src/main/java/com/project/library/domain/User.java
+++ src/main/java/com/project/library/domain/User.java
@@ -1,4 +1,38 @@
 package com.project.library.domain;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "lastName")
+    private String lastName;
+
+    @NotNull
+    @Column (name = "createdAccountData")
+    private LocalDate createdAccount;
+
+    @OneToMany(
+            targetEntity = BookHire.class,
+            mappedBy = "user",
+            fetch = FetchType.EAGER
+    )
+    private List<BookHire> booksHire;
 }
