acafcf68b8162331b12e19730333da7c6ca4a089
kurkova <kurkiewiczpaula@gmail.com>
2019-12-17 15:57:22 +0100

    Refactoring

--- src/main/java/com/project/library/domain/Book.java
+++ src/main/java/com/project/library/domain/Book.java
@@ -1,4 +1,36 @@
 package com.project.library.domain;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name ="id")
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "title_id")
+    private BookTitle bookTitle;
+
+    @NotNull
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private BookStatus bookStatus;
+
+    @OneToMany(
+            targetEntity = com.project.library.domain.BookHire.class,
+            mappedBy = "book",
+            fetch = FetchType.EAGER
+    )
+    private List<BookHire> bookHires;
 }
