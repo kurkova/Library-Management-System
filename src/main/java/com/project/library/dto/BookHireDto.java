a8e1be34f026b7ab93a680b52e4d70c4e798f762
kurkova <kurkiewiczpaula@gmail.com>
2019-12-17 15:58:22 +0100

    Refactoring

--- src/main/java/com/project/library/dto/BookHireDto.java
+++ src/main/java/com/project/library/dto/BookHireDto.java
@@ -3,13 +3,14 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class BookHireDto {
     private Long id;
-    private Long userId;
-    private Long bookId;
+    private Long user;
+    private Long book;
     private LocalDate rentalDate;
     private LocalDate returnDate;
 }
