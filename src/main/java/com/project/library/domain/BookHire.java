c094e96cea22bed47b341267b8fa5c050b3cf4ee
kurkova <kurkiewiczpaula@gmail.com>
2019-12-17 17:26:12 +0100

    Refactoring

--- src/main/java/com/project/library/domain/BookHire.java
+++ src/main/java/com/project/library/domain/BookHire.java
@@ -1,4 +1,34 @@
 package com.project.library.domain;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "book_hires")
 public class BookHire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @NotNull
+    @Column(name = "rental_date")
+    private LocalDate rentalDate;
+
+    @NotNull
+    @Column(name = "return_date")
+    private LocalDate returnDate;
 }
