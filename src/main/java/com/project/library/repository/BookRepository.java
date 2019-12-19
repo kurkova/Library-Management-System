2ea0fae3ef2da3495c0ba66ba234b0151f9dc400
kurkova <kurkiewiczpaula@gmail.com>
2019-12-19 09:30:51 +0100

    Add findById repository

--- src/main/java/com/project/library/repository/BookRepository.java
+++ src/main/java/com/project/library/repository/BookRepository.java
@@ -1,15 +1,13 @@
 package com.project.library.repository;
 import com.project.library.domain.Book;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
-@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
-
     @Override
     List<Book> findAll();
 
     @Override
-    Book save (Book bookCopy);
+    Optional<Book> findById(Long bookId);
 }
