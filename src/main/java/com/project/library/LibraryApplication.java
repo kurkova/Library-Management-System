738ba939ede0b95bcad0e381ae22c93326584b03
kurkova <kurkiewiczpaula@gmail.com>
2019-12-19 08:17:50 +0100

    Refactoring

--- src/main/java/com/project/library/LibraryApplication.java
+++ src/main/java/com/project/library/LibraryApplication.java
@@ -1,9 +1,7 @@
 package com.project.library;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class LibraryApplication {
