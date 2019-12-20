31ffd091e0c86f969e91c5b0cc167230e718e9f1
kurkova <kurkiewiczpaula@gmail.com>
2019-12-20 10:05:42 +0100

    Create findByUserIdAndBookId

--- src/main/java/com/project/library/repository/BookHireRepository.java
+++ src/main/java/com/project/library/repository/BookHireRepository.java
@@ -1,11 +1,15 @@
 package com.project.library.repository;
 import com.project.library.domain.BookHire;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface BookHireRepository extends CrudRepository <BookHire, Long> {
 
-    Optional<BookHire> findUsersBookHire (Long bookHireId, Long userId);
+public interface BookHireRepository extends CrudRepository <BookHire, Long> {
+    @Override
+    List<BookHire> findAll();
+
+
+    Optional<BookHire> findByUserIdAndBookId(Long userId, Long BookId);
 }
